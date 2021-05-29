c596f346a1dc5e32b13547da8d6d063a30ba872d
Bora <yokunjon@gmail.com>
2021-05-29 10:52:43 +0300

    Implement wrapper named Query over QueryParser

--- src/internal/store/mod.rs
+++ src/internal/store/mod.rs
@@ -1,5 +1,7 @@
 pub mod database;
 pub mod collection;
+pub mod query;
 
 pub use database::{ Database, key_controls };
 pub use collection::Collection;
+pub use query::Query;
---
+++ src/internal/store/query.rs
@@ -0,0 +1,19 @@
+use crate::internal::parser::{Parser, JSONParser};
+use crate::internal::parser::query_parser::{QueryParser, LogicalOperation};
+
+pub enum Query {
+    All,
+    By(LogicalOperation)
+}
+
+impl Query {
+    pub fn new(json: String) -> Query {
+        if let "{}" = json.as_str() {
+            Query::All
+        } else {
+            let parser = JSONParser::new(json);
+            let parser = QueryParser::new(parser.parse());
+            Query::By(parser.parse())
+        }
+    }
+}
